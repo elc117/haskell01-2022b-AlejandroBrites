748a743162fae17d7fe2958223dc9a2e43535d11
AlejandroBrites <104328707+AlejandroBrites@users.noreply.github.com>
2023-01-05 12:13:52 -0300

    Add files via upload

---
+++ haskell01parte1.hs
@@ -0,0 +1,35 @@
+sumSquares :: Int -> Int -> Int
+sumSquares x y = (x²) + (y²)
+
+circleArea :: Float -> Float
+circleArea x = pi*(x²)
+
+age :: Int -> Int -> Int
+age x y = y-x
+
+isElderly :: Int -> Bool
+isElderly x = x > 65
+
+htmlItem :: String -> String
+htmlItem x = "<li>" ++ x ++ "</li>"
+
+startsWithA :: String -> Bool
+startsWithA x = head x == 'A'
+
+isVerb :: String -> Bool
+isVerb x = last x == 'r'
+
+isVowel :: Char -> Bool
+isVolwel x 
+   | x == 'a'  = 1
+   | x == 'e'  = 1
+   | x == 'i'  = 1
+   | x == 'o'  = 1
+   | x == 'u'  = 1
+   | otherwise = 0
+
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads list1 list2 = (head list1) == (head list2)
+
+isVowel2 :: Char -> Bool
+isVowel2 x = elem x ["AEIOUaeiou"]
---
+++ haskell01parte2.hs
@@ -0,0 +1,55 @@
+htmlItem :: String -> String
+htmlItem x = "<li>" ++ x ++ "</li>"
+
+itemize :: [String] -> [String]
+itemize x = map htmlItem x 
+
+
+isVowel :: Char -> Bool
+isVolwel x 
+   | x == 'a'  = 1
+   | x == 'e'  = 1
+   | x == 'i'  = 1
+   | x == 'o'  = 1
+   | x == 'u'  = 1
+   | otherwise = 0
+   
+onlyVowels :: String -> String
+onlyVowels x = filter isVowel x
+
+
+isElderly :: Int -> Bool
+isElderly x = x > 65
+
+onlyElderly :: [Int] -> [Int]
+onlyElderly x = filter isElderly x
+
+
+isLongWord :: String -> Bool
+isLongWord s = length s > 10
+
+onlyLongWords :: [String] -> [String]
+onlyLongWords x = filter isLongWord x
+
+isEven :: Int -> Bool
+isEven x = mod x 2 == 0
+
+onlyEven :: [Int] -> [Int]
+onlyEven x = filter isEven x
+
+
+onlyBetween60and80
+
+
+space :: Char -> Bool
+space c = c == ' '
+
+countSpaces :: String -> Int
+countSpaces x = length (filter space x)
+
+
+circleArea :: Float -> Float
+circleArea x = pi*(x²)
+
+calcAreas :: [Float] -> [Float]
+clacAreas x = map circleArea x
