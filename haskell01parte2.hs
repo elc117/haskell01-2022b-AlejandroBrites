6406eed760386174da1498aae7a432a6f9ead12a
AlejandroBrites <104328707+AlejandroBrites@users.noreply.github.com>
2023-01-07 22:41:05 -0300

    Update haskell01parte2.hs

--- haskell01parte2.hs
+++ haskell01parte2.hs
@@ -1,52 +1,56 @@
+-- Atividade 1
 htmlItem :: String -> String
 htmlItem x = "<li>" ++ x ++ "</li>"
 
 itemize :: [String] -> [String]
 itemize x = map htmlItem x 
 
-
+-- Atividade 2
 isVowel :: Char -> Bool
 isVowel x = elem x "AEIOUaeiou"
    
 onlyVowels :: String -> String
 onlyVowels x = filter isVowel x
 
-
+-- Atividade 3
 isElderly :: Int -> Bool
 isElderly x = x > 65
 
 onlyElderly :: [Int] -> [Int]
 onlyElderly x = filter isElderly x
 
+-- Atividade 4
+-- Não houve atividade 4
 
+-- Atividade 5
 isLongWord :: String -> Bool
 isLongWord s = length s > 10
 
 onlyLongWords :: [String] -> [String]
 onlyLongWords x = filter isLongWord x
 
-
+-- Atividade 6
 isEven :: Int -> Bool
 isEven x = mod x 2 == 0
 
 onlyEven :: [Int] -> [Int]
 onlyEven x = filter isEven x
 
-
+-- Atividade 7
 between60and80 :: Int -> Bool
 between60and80 x = (x > 60) && (x < 80)
 
 onlyBetween60and80
 onlyBetween60and80 x = filter between60and80 x
 
-
+-- Atividade 8
 space :: Char -> Bool
 space c = c == ' '
 
 countSpaces :: String -> Int
 countSpaces x = length (filter space x)
 
-
+-- Atividade 9
 circleArea :: Float -> Float
 circleArea x = pi*x*x
 
